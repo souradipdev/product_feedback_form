3858bc54967fae205b5bc499678ff54ec9a9ba90
souradipdev <22052939@kiit.ac.in>
2024-08-04 21:05:03 +0530

    Created database model

--- src/model/FormModel.tsx
+++ src/model/FormModel.tsx
@@ -1,2 +1,45 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
+
+interface IForm extends Document {
+  name: string;
+  email: string;
+  phone: string;
+  satisfactionRating: number;
+  easeOfUse: number;
+  featureCompleteness: number;
+  recommend: "yes" | "no";
+  customerRecomendation: string;
+}
+
+
+const FormSchema: Schema<IForm> = new Schema({
+  name: {
+    type: String, required: true
+  },
+  email: {
+    type: String, required: true
+  },
+  phone: {
+    type: String, required: false
+  },
+  satisfactionRating: {
+    type: Number, required: true
+  },
+  easeOfUse: {
+    type: Number, required: true
+  },
+  featureCompleteness: {
+    type: Number, required: true
+  },
+  recommend: {
+    type: String, enum: ["yes", "no"], required: true
+  },
+  customerRecomendation: {
+    type: String
+  }
+}, { timestamps: true });
+
+
+const FormModel = (mongoose.models.IForm as mongoose.Model<IForm>  ||mongoose.model<IForm>('Form', FormSchema));
+export default FormModel;
